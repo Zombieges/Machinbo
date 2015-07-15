45893dff0d268b3008e542103c41da2756091940
legobrokkori <ralirulego103@gmail.com>
2015-07-15 19:04:07 +0900

    AppDelegate

--- Machinbo/AppDelegate.swift
+++ Machinbo/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
-        GMSServices.provideAPIKey("AIzaSyDrB6jLqJERRanVLE28MnmGtZj7S-xjNnU");
         
         // Override point for customization after application launch.
         return true

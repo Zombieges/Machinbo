08de67ad454e3f35919b26276877bf09a9af0334
legobrokkori <ralirulego103@gmail.com>
2015-07-23 20:10:19 +0900

    comitt

---
+++ Machinbo/PhotoDIsplayViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PhotoDIsplayViewController.swift
+//  Machinbo
+//
+//  Created by Kazuhiro Watanabe on 2015/07/20.
+//  Copyright (c) 2015年 Zombieges. All rights reserved.
+//
+
+import Foundation
---
+++ Machinbo/SelectPhotoViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SelectPhoto.swift
+//  Machinbo
+//
+//  Created by Kazuhiro Watanabe on 2015/07/20.
+//  Copyright (c) 2015年 Zombieges. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SelectPhoto: UIViewController {
+    
+    var mySelectedImage: UIImage!
+    var mySelectedImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        
+        self.edgesForExtendedLayout = UIRectEdge.None
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        setImage()
+    }
+    
+    /**
+    選択された画像をUIImageViewにセットする.
+    */
+    func setImage(){
+        self.title = "Selected Image"
+        
+        mySelectedImageView = UIImageView(frame: self.view.bounds)
+        mySelectedImageView.contentMode = UIViewContentMode.ScaleAspectFit
+        mySelectedImageView.image = mySelectedImage
+        self.view.addSubview(mySelectedImageView)
+    }
+    
+}

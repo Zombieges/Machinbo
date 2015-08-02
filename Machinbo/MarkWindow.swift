f6c98c1c369f86ecd234584eb47b0f74a40e0f7a
t.shimizu <yabecchi.ext@gmail.com>
2015-08-02 11:46:20 +0900

    commit

---
+++ Machinbo/MarkWindow.swift
@@ -0,0 +1,19 @@
+//
+//  MarkWindow.swift
+//  Machinbo
+//
+//  Created by ExtYabecchi on 2015/08/01.
+//  Copyright (c) 2015年 Zombieges. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MarkWindow: UIView {
+    
+    @IBOutlet weak var ProfileImage: UIImageView!
+    @IBOutlet weak var Name: UILabel!
+    
+    @IBOutlet weak var Detail: UILabel!
+    
+}

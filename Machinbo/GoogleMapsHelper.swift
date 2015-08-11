b1a554e332c779b2ef65226da7d1e86647aa8409
t.shimizu <yabecchi.ext@gmail.com>
2015-08-11 20:50:49 +0900

    commit

--- Machinbo/GoogleMapsHelper.swift
+++ Machinbo/GoogleMapsHelper.swift
@@ -8,25 +8,33 @@
 
 import Foundation
 import Parse
-
 import GoogleMaps
-import Parse
 
 class GoogleMapsHelper {
 
     class func setUserMarker(map: GMSMapView, userObjects: [PFObject]) {
+        
         for user in userObjects {
             let UserID = user.objectForKey("UserID") as! String
 
             let geoPoint : PFGeoPoint
             if let tempGeopoint = (user.objectForKey("GPS") as? PFGeoPoint) {
                 geoPoint = tempGeopoint
+                
             } else {
                 geoPoint = PFGeoPoint(latitude: 0, longitude: 0)
             }
 
-            let location = CLLocationCoordinate2DMake(geoPoint.latitude, geoPoint.longitude)
-
+            var marker = GMSMarker()
+            marker.position = CLLocationCoordinate2DMake(geoPoint.latitude, geoPoint.longitude)
+            marker.appearAnimation = kGMSMarkerAnimationPop
+            marker.map = map
+            marker.userData = user
+            
+            NSLog("UserID================>" + UserID)
+            
+            //↓表示したMAP内のみでピンを立てるロジック
+            /*
             if map.projection.containsCoordinate(location) {
                 var marker = GMSMarker()
                 marker.position = CLLocationCoordinate2DMake(geoPoint.latitude, geoPoint.longitude)
@@ -41,7 +49,7 @@ class GoogleMapsHelper {
             } else {
                 NSLog("★★★" + UserID)
                 continue
-            }
+            }*/
         }
     }
 }
--- Machinbo/MapViewController.swift
+++ Machinbo/MapViewController.swift
@@ -11,26 +11,18 @@ import GoogleMaps
 import CoreLocation
 import Parse
 
-let NavigationBarHeight = 44, StatusBarHeight = 20, SearchBarHeight = 44
-
 class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
     
     var gmaps: GMSMapView?
     var markers : [GMSMarker] = []
     
-    //現在地の位置情報取得
-    let locationManager = CLLocationManager()
-    // 取得した緯度を保持
+    var lm : CLLocationManager!
     var longitude: CLLocationDegrees!
-    // 取得した軽度を保持
     var latitude: CLLocationDegrees!
     
     @IBOutlet weak var mapViewContainer: UIView!
     
-    @IBOutlet weak var GPSUpdateContainer: UIButton!
-    
-    var geoPoint = PFGeoPoint()
-    var feedItems: [PFObject] = []
+    private var updateGeoPoint : ZFRippleButton!
     
     // CLLocationManagerDelegateを継承すると、init()が必要になる
     required init(coder aDecoder: NSCoder) {
@@ -43,32 +35,23 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mapViewContainer.hidden = true
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-
-        /*
+        //updateGeoPoint = ZFRippleButton(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        //self.view.addSubview(updateGeoPoint)
+        
+        
+        lm = CLLocationManager()
+        lm.delegate = self
+        lm.desiredAccuracy = kCLLocationAccuracyBest
+        lm.distanceFilter = 100
+        
         // セキュリティ認証のステータスを取得
         let status = CLLocationManager.authorizationStatus()
         if status == CLAuthorizationStatus.NotDetermined {
-        println("didChangeAuthorizationStatus:\(status)");
-        // まだ承認が得られていない場合は、認証ダイアログを表示
-        self.locationManager.requestAlwaysAuthorization()
+            // まだ承認が得られていない場合は、認証ダイアログを表示
+            self.lm.requestWhenInUseAuthorization()
         }
-        */
         
-        locationManager.delegate = self
-        // 取得精度の設定
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        // 取得頻度の設定
-        locationManager.distanceFilter = 100
-        
-        locationManager.requestWhenInUseAuthorization()
-        
-        // 現在位置の取得
-        locationManager.startUpdatingLocation()
+        lm.startUpdatingLocation()
         
     }
     
@@ -101,21 +84,15 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
         super.didReceiveMemoryWarning()
     }
     
+    
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
         latitude = locations.first!.coordinate.latitude
         longitude = locations.first!.coordinate.longitude
         
-        // 取得した緯度・経度をLogに表示
         NSLog("位置情報取得成功！-> latiitude: \(latitude) , longitude: \(longitude)")
         
         var target = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
-        
-        var camera = GMSCameraPosition(
-            target: target,
-            zoom: 15,
-            bearing: 0,
-            viewingAngle: 0
-        )
+        var camera = GMSCameraPosition(target: target, zoom: 13, bearing: 0, viewingAngle: 0)
         
         gmaps = GMSMapView()
         if let map = gmaps {
@@ -138,22 +115,12 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
             //self.markers = []
             //self.markers.append(myMarker)
             
-            //
-            var userinfo = ParseHelper.getUserInfo()
+            //現在の自分の表示範囲から50kmの範囲、100件のデータを取得する
+            var userinfo = ParseHelper.getNearUserInfomation(target)
             GoogleMapsHelper.setUserMarker(map, userObjects: userinfo)
         }
-
-        
-        //消しちゃダメ！
-        /*
-        var current_location = locations[0] as? CLLocation
-        if let current_cordinate = (current_location!.coordinate) as CLLocationCoordinate2D?{
-        self.updateGoogleMapView(current_cordinate)
-        }
-        */
         
         manager.stopUpdatingLocation()
-        
     }
     
     func mapView(mapView: GMSMapView!, markerInfoWindow marker: GMSMarker!) -> UIView! {
@@ -167,26 +134,6 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
         return markWindow
     }
     
-    private func reloadMarkers(){
-        
-        //DBから再度マーカーを取得する
-        
-        //地図が表示外になった場合、マーカーを非表示にする
-        for marker in self.markers {
-            if self.gmaps!.projection.containsCoordinate(marker.position) == false {
-                marker.map = nil
-                NSLog("マーカー非表示")
-                
-            } else {
-                marker.map = self.gmaps
-                NSLog("マーカー表示")
-                
-            }
-        }
-        
-        markers = []
-    }
-    
     func mapView(mapView: GMSMapView!, didTapMarker marker: GMSMarker!) -> Bool {
         return false
     }
@@ -196,11 +143,7 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
         NSLog("位置情報取得失敗")
     }
     
-    func mapView(mapView: GMSMapView!, idleAtCameraPosition position: GMSCameraPosition!) {
-        self.reloadMarkers()
-    }
-    
-    @IBAction func updateGPS(sender: AnyObject) {
+    /*@IBAction func updateGPS(sender: AnyObject) {
         var geoPoint = PFGeoPoint(latitude: latitude, longitude: longitude);
         
         let gpsMark = PFObject(className: "UserInfo")
@@ -210,6 +153,6 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
             NSLog("GPS情報登録成功")
         }
         
-    }
+    }*/
 }
 

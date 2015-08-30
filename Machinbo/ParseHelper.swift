46fabbfdc0c46807cafd9a8938410c7aea4aefa6
t.shimizu <yabecchi.ext@gmail.com>
2015-08-30 12:11:14 +0900

    commit

--- Machinbo/ParseHelper.swift
+++ Machinbo/ParseHelper.swift
@@ -22,6 +22,26 @@ class ParseHelper {
         Parse.enableLocalDatastore()
         Parse.setApplicationId(parseAppIdKey, clientKey:parseClientKey)
         PFAnalytics.trackAppOpenedWithLaunchOptions(launchOptions)
+        
+        //UserInfo Search
+        var query = PFQuery(className: "UserInfo")
+        query.whereKey("UserID", containsString: "015CD7AC-F749-479E-B350-B1C41A832EED")
+        query.findObjectsInBackgroundWithBlock { (objects, error) in
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            
+            if error == nil {
+                let currentController = storyboard.instantiateViewControllerWithIdentifier("profile") as? ProfileViewController
+                //self.window?.rootViewController = currentController
+                
+                return
+                
+            } else {
+                let currentController = storyboard.instantiateViewControllerWithIdentifier("map") as? MapViewController
+                //self.window?.rootViewController = currentController
+            }
+        }
+
     }
     
     /*class func getUserInfo(userID:String) -> PFObject {
@@ -40,27 +60,11 @@ class ParseHelper {
     }*/
     
     class func getNearUserInfomation(myLocation: CLLocationCoordinate2D) -> [PFObject] {
-        //50km圏内、近くから100件取得
         var myGeoPoint = PFGeoPoint(latitude: myLocation.latitude, longitude: myLocation.longitude)
         
         var query = PFQuery(className: "UserInfo")
         query.whereKey("GPS", nearGeoPoint: myGeoPoint, withinKilometers: 50.0)
         query.limit = 100
         return query.findObjects() as! [PFObject]
-    }
-    
-    class func setUserInfomation(userID: String) {
-        //新規ユーザー登録
-        let info = PFObject(className: "UserInfo")
-        info["UserID"] = userID
-        info["Name"] = ""
-        info["Gender"] = 0
-        info["Age"] = ""
-        info["Comment"] = ""
-        info["GPS"] = nil
-        info["MarkTime"] = nil
-        info.saveInBackgroundWithBlock { (success: Bool, error: NSError?) -> Void in
-            NSLog("ユーザー初期登録成功")
-        }
     }
 }

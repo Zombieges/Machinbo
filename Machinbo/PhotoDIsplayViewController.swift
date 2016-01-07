c134cd24831c322579bd5835289a2d0cd2369e5a
t.shimizu <yabecchi.ext@gmail.com>
2016-01-07 22:15:01 +0900

    commit

--- Machinbo/PhotoDIsplayViewController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PhotoDIsplayViewController.swift
-//  Machinbo
-//
-//  Created by Kazuhiro Watanabe on 2015/07/20.
-//  Copyright (c) 2015年 Zombieges. All rights reserved.
-//
-
-import Foundation
